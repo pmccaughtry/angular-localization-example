5757b6eeaf83c4bb91c35f7397b3574470a66f59
Paul McCaughtry <paul.mccaughtry@gmail.com>
2015-06-26 00:39:34 -0500

    removed comments

--- app/js/services/localization.js
+++ app/js/services/localization.js
@@ -1,26 +1,7 @@
 'use strict';
-/*
- * Use of functional strict mode causes scoping issues when declaring the function or creating as an expression.
- * The use of angular.module (below) is needed to remedy.
- */
 
-/**
- Service to localize strings
-
- @module services.localization
- @requires ngResource
- */
 angular.module('localization')
 
-/**
- Provides service to localize strings in the UI
-
- @class Localize
- @constructor
- @example
- Localize()
- @return {String} that is localized to target language
- */
 .factory('Localize', ['$resource', function ($resource) {
   var nav = navigator,
     lang = nav.language ? nav.language.toLowerCase() : nav.browserLanguage.toLowerCase(),
