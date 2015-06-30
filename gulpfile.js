74245fac904bea1bf84b961c9ae6de6ed3b73ec4
Paul McCaughtry <paul.mccaughtry@caringo.com>
2015-06-30 09:24:05 -0500

    Added tasks to update webdriver-manager and to start webdriver-mananger as a stand-alone server.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp'),
     karma = require('karma').server,
-    protractor = require('gulp-protractor').protractor;
+    protractor = require('gulp-protractor').protractor,
+	webdriver_standalone = require('gulp-protractor').webdriver_standalone,
+	webdriver_update = require('gulp-protractor').webdriver_update;
 
 gulp.task('karma', function (done) {
   karma.start({
@@ -8,7 +10,16 @@ gulp.task('karma', function (done) {
   }, done);
 });
 
-gulp.task('protractor', function (done) {
+// Downloads the selenium webdriver
+// Make sure to update /config/protractor.conf.js with path to selenium.jar
+gulp.task('webdriver_update', webdriver_update);
+
+// Start the standalone selenium server
+// NOTE: This is not needed if you reference the
+// seleniumServerJar in your protractor.conf.js
+gulp.task('webdriver_standalone', webdriver_standalone);
+
+gulp.task('protractor', ['webdriver_update'], function (done) {
   gulp.src(['tests/e2e/*_spec.js'])
     .pipe(protractor({
       configFile: 'config/protractor.conf.js'
