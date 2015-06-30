239fc0fbcbfdc52fe7c9e2d6043bf114cd095f23
Paul McCaughtry <paul.mccaughtry@gmail.com>
2015-06-29 23:56:30 -0500

    Added unit tests for routes and updated unit test screenshot.

---
+++ test/unit/routes.spec.js
@@ -0,0 +1,70 @@
+'use strict';
+
+describe('Application Routes', function () {
+
+  var location, rootScope, route, $window;
+
+  beforeEach(function () {
+    module('localization');
+
+    inject(function (_$location_, _$rootScope_, _$route_) {
+      location = _$location_;
+      rootScope = _$rootScope_;
+      route = _$route_;
+    });
+  });
+
+  describe('Login Route', function () {
+    beforeEach(inject(function ($httpBackend) {
+      $httpBackend.expectGET('views/login.html').respond(200, 'login HTML');
+    }));
+
+    it('should take user to login screen', function () {
+      location.path('/login');
+      rootScope.$digest();
+      expect(location.path()).toBe('/login');
+    });
+
+    it('should use LoginController controller', function () {
+      location.path('/taco-salads');
+      rootScope.$digest();
+      expect(route.current.controller).toBe('LoginController as lCtrl');
+    });
+  });
+
+  describe('Welcome Route', function () {
+    beforeEach(inject(function ($httpBackend) {
+      $httpBackend.expectGET('views/welcome.html').respond(200, 'welcome HTML');
+    }));
+
+    it('should take user to welcome screen', function () {
+      location.path('/welcome');
+      rootScope.$digest();
+      expect(location.path()).toBe('/welcome');
+    });
+
+    it('should not have a controller', function () {
+      location.path('/welcome');
+      rootScope.$digest();
+      expect(route.current.controller).toBeUndefined();
+    });
+  });
+
+  describe('Undefined routes', function () {
+    beforeEach(inject(function ($httpBackend) {
+      $httpBackend.expectGET('views/login.html').respond(200, 'login HTML');
+    }));
+
+    it('should redirect to the index/login page when an unrecognized route is passed (i.e. /taco-salads)', function () {
+      location.path('/taco-salads');
+      rootScope.$digest();
+      expect(location.path()).toBe('/login');
+    });
+
+    it('should use LoginController controller', function () {
+      location.path('/taco-salads');
+      rootScope.$digest();
+      expect(route.current.controller).toBe('LoginController as lCtrl');
+    });
+  });
+});
