27b82165b99d0e7de8bfdf10fa8bc731b1066466
Paul McCaughtry <paul.mccaughtry@caringo.com>
2015-07-01 22:04:40 -0500

    Added French and German localized strings

--- app/js/services/localization.js
+++ app/js/services/localization.js
@@ -2,21 +2,27 @@
 
 angular.module('localization')
 
-.factory('Localize', ['$resource', function ($resource) {
-  var nav = navigator,
-    lang = nav.language ? nav.language.toLowerCase() : nav.browserLanguage.toLowerCase(),
-    file;
+    .factory('Localize', ['$resource', function ($resource) {
+        var nav = navigator,
+            lang = nav.language ? nav.language.toLowerCase() : nav.browserLanguage.toLowerCase(),
+            file;
 
-  switch (true) {
-    case /es/.test(lang):
-      file = 'localization/labels.es-MX.json';
-      break;
-    default:
-      file = 'localization/labels.en-US.json';
-      break;
-  }
+        switch (true) {
+            case /es/.test(lang):
+                file = 'localization/labels.es-MX.json';
+                break;
+            case /de/.test(lang):
+                file = 'localization/labels.de-DE.json';
+                break;
+            case /fr/.test(lang):
+                file = 'localization/labels.fr-FR.json';
+                break;
+            default:
+                file = 'localization/labels.en-US.json';
+                break;
+        }
 
-  return $resource(file, {}, {
-    query: { method: 'GET', params: {}, isArray: true }
-  });
-}]);
+        return $resource(file, {}, {
+            query: { method: 'GET', params: {}, isArray: true }
+        });
+    }]);
--- test/unit/services/localization.spec.js
+++ test/unit/services/localization.spec.js
@@ -1,127 +1,241 @@
 'use strict';
 
 describe('Localization service', function () {
-  var scope, $httpBackend, lang, file;
+    var scope, $httpBackend, lang, file;
 
-  beforeEach(module('localization'));
+    beforeEach(module('localization'));
 
-  describe('English string localization', function () {
-    beforeEach(function () {
-      inject(function ($rootScope, _$httpBackend_, Localize) {
-        lang = 'en-US';
+    describe('English string localization', function () {
+        beforeEach(function () {
+            inject(function ($rootScope, _$httpBackend_, Localize) {
+                lang = 'en-US';
 
-        switch (true) {
-          case /es/.test(lang):
-            file = 'localization/labels.es-MX.json';
-            break;
-          default:
-            file = 'localization/labels.en-US.json';
-            break;
-        }
+                switch (true) {
+                    case /es/.test(lang):
+                        file = 'localization/labels.es-MX.json';
+                        break;
+                    case /de/.test(lang):
+                        file = 'localization/labels.de-DE.json';
+                        break;
+                    case /fr/.test(lang):
+                        file = 'localization/labels.fr-FR.json';
+                        break;
+                    default:
+                        file = 'localization/labels.en-US.json';
+                        break;
+                }
 
-        $httpBackend = _$httpBackend_;
-        $httpBackend.expectGET(file).respond({
-          name: "Name",
-          gender: "Gender"
-        });
-        scope = $rootScope.$new();
-        scope.labels = Localize.get();
-      });
-    });
-
-    afterEach(function () {
-      lang = null;
-    });
-
-    it('should return localized objects', function () {
-      expect(Object.keys(scope.labels).length).toEqual(2);
-    });
-
-    it('should return English string when en-US is detected', function () {
-      expect(file).toBe('localization/labels.en-US.json');
-    });
-  });
-
-  describe('Spanish string localization', function () {
-    beforeEach(function () {
-      inject(function ($rootScope, _$httpBackend_, Localize) {
-        lang = 'es-MX';
-
-        switch (true) {
-          case /es/.test(lang):
-            file = 'localization/labels.es-MX.json';
-            break;
-          default:
-            file = 'localization/labels.en-US.json';
-            break;
-        }
-
-        // required a new digest cycle to override the lang variable passed from the service
-        $rootScope.$digest();
-
-        $httpBackend = _$httpBackend_;
-        $httpBackend.expectGET(file).respond({
-          name: "Name",
-          gender: "Gender"
+                $httpBackend = _$httpBackend_;
+                $httpBackend.expectGET(file).respond({
+                    name: "Name",
+                    gender: "Gender"
+                });
+                scope = $rootScope.$new();
+                scope.labels = Localize.get();
+            });
         });
 
-        scope = $rootScope.$new();
-        scope.labels = Localize.get();
-
-      });
-    });
-
-    afterEach(function () {
-      lang = null;
-    });
-
-    it('should return localized objects', function () {
-      expect(Object.keys(scope.labels).length).toEqual(2);
-    });
-
-    it('should return Spanish string when es-MX is detected', function () {
-      expect(file).toBe('localization/labels.es-MX.json');
-    });
-  });
-
-  describe('Non supported language string localization', function () {
-    beforeEach(function () {
-      inject(function ($rootScope, _$httpBackend_, Localize) {
-        lang = 'cn-CN';
-
-        switch (true) {
-          case /es/.test(lang):
-            file = 'localization/labels.es-MX.json';
-            break;
-          default:
-            file = 'localization/labels.en-US.json';
-            break;
-        }
-
-        // required a new digest cycle to override the lang variable passed from the service
-        $rootScope.$digest();
-
-        $httpBackend = _$httpBackend_;
-        $httpBackend.expectGET(file).respond({
-          name: "Name",
-          gender: "Gender"
+        afterEach(function () {
+            lang = null;
         });
 
-        scope = $rootScope.$new();
-        scope.labels = Localize.get();
-      });
+        it('should return localized objects', function () {
+            expect(Object.keys(scope.labels).length).toEqual(2);
+        });
+
+        it('should return English string when en-US is detected', function () {
+            expect(file).toBe('localization/labels.en-US.json');
+        });
     });
 
-    afterEach(function () {
-      lang = null;
+    describe('Spanish string localization', function () {
+        beforeEach(function () {
+            inject(function ($rootScope, _$httpBackend_, Localize) {
+                lang = 'es-MX';
+
+                switch (true) {
+                    case /es/.test(lang):
+                        file = 'localization/labels.es-MX.json';
+                        break;
+                    case /de/.test(lang):
+                        file = 'localization/labels.de-DE.json';
+                        break;
+                    case /fr/.test(lang):
+                        file = 'localization/labels.fr-FR.json';
+                        break;
+                    default:
+                        file = 'localization/labels.en-US.json';
+                        break;
+                }
+
+                // required a new digest cycle to override the lang variable passed from the service
+                $rootScope.$digest();
+
+                $httpBackend = _$httpBackend_;
+                $httpBackend.expectGET(file).respond({
+                    name: "Name",
+                    gender: "Gender"
+                });
+
+                scope = $rootScope.$new();
+                scope.labels = Localize.get();
+
+            });
+        });
+
+        afterEach(function () {
+            lang = null;
+        });
+
+        it('should return localized objects', function () {
+            expect(Object.keys(scope.labels).length).toEqual(2);
+        });
+
+        it('should return Spanish string when es-MX is detected', function () {
+            expect(file).toBe('localization/labels.es-MX.json');
+        });
     });
 
-    it('should return localized objects', function () {
-      expect(Object.keys(scope.labels).length).toEqual(2);
+    describe('German string localization', function () {
+        beforeEach(function () {
+            inject(function ($rootScope, _$httpBackend_, Localize) {
+                lang = 'de-DE';
+
+                switch (true) {
+                    case /es/.test(lang):
+                        file = 'localization/labels.es-MX.json';
+                        break;
+                    case /de/.test(lang):
+                        file = 'localization/labels.de-DE.json';
+                        break;
+                    case /fr/.test(lang):
+                        file = 'localization/labels.fr-FR.json';
+                        break;
+                    default:
+                        file = 'localization/labels.en-US.json';
+                        break;
+                }
+
+                // required a new digest cycle to override the lang variable passed from the service
+                $rootScope.$digest();
+
+                $httpBackend = _$httpBackend_;
+                $httpBackend.expectGET(file).respond({
+                    name: "Name",
+                    gender: "Gender"
+                });
+
+                scope = $rootScope.$new();
+                scope.labels = Localize.get();
+
+            });
+        });
+
+        afterEach(function () {
+            lang = null;
+        });
+
+        it('should return localized objects', function () {
+            expect(Object.keys(scope.labels).length).toEqual(2);
+        });
+
+        it('should return German string when de-DE is detected', function () {
+            expect(file).toBe('localization/labels.de-DE.json');
+        });
     });
 
-    it('should return English string when language is not supported', function () {
-      expect(file).toBe('localization/labels.en-US.json');
+    describe('French string localization', function () {
+        beforeEach(function () {
+            inject(function ($rootScope, _$httpBackend_, Localize) {
+                lang = 'fr-FR';
+
+                switch (true) {
+                    case /es/.test(lang):
+                        file = 'localization/labels.es-MX.json';
+                        break;
+                    case /de/.test(lang):
+                        file = 'localization/labels.de-DE.json';
+                        break;
+                    case /fr/.test(lang):
+                        file = 'localization/labels.fr-FR.json';
+                        break;
+                    default:
+                        file = 'localization/labels.en-US.json';
+                        break;
+                }
+
+                // required a new digest cycle to override the lang variable passed from the service
+                $rootScope.$digest();
+
+                $httpBackend = _$httpBackend_;
+                $httpBackend.expectGET(file).respond({
+                    name: "Name",
+                    gender: "Gender"
+                });
+
+                scope = $rootScope.$new();
+                scope.labels = Localize.get();
+
+            });
+        });
+
+        afterEach(function () {
+            lang = null;
+        });
+
+        it('should return localized objects', function () {
+            expect(Object.keys(scope.labels).length).toEqual(2);
+        });
+
+        it('should return French string when es-MX is detected', function () {
+            expect(file).toBe('localization/labels.fr-FR.json');
+        });
     });
-  });
+
+    describe('Non supported language string localization', function () {
+        beforeEach(function () {
+            inject(function ($rootScope, _$httpBackend_, Localize) {
+                lang = 'cn-CN';
+
+                switch (true) {
+                    case /es/.test(lang):
+                        file = 'localization/labels.es-MX.json';
+                        break;
+                    case /de/.test(lang):
+                        file = 'localization/labels.de-DE.json';
+                        break;
+                    case /fr/.test(lang):
+                        file = 'localization/labels.fr-FR.json';
+                        break;
+                    default:
+                        file = 'localization/labels.en-US.json';
+                        break;
+                }
+
+                // required a new digest cycle to override the lang variable passed from the service
+                $rootScope.$digest();
+
+                $httpBackend = _$httpBackend_;
+                $httpBackend.expectGET(file).respond({
+                    name: "Name",
+                    gender: "Gender"
+                });
+
+                scope = $rootScope.$new();
+                scope.labels = Localize.get();
+            });
+        });
+
+        afterEach(function () {
+            lang = null;
+        });
+
+        it('should return localized objects', function () {
+            expect(Object.keys(scope.labels).length).toEqual(2);
+        });
+
+        it('should return English string when language is not supported', function () {
+            expect(file).toBe('localization/labels.en-US.json');
+        });
+    });
 });
